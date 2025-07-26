fcb61946145e513d3805a6a51956141cc9c05e99
Yinpan Jia <chrisjia54@gmail.com>
2025-07-26 21:13:53 +1200

    Update deploy-aisuggestion.sh

--- deploy-aisuggestion.sh
+++ deploy-aisuggestion.sh
@@ -11,16 +11,16 @@ git pull origin   $BRANCH
 
 echo "✅ git pull finished, entry directory：$LOCAL_DIR"
 echo "🐳 start build Docker image..."
-if ! docker info | grep -q "Username: your username"; then    # you should use your own username of fly or other platform
+if ! docker info | grep -q "Username: yourusername"; then    # you should use your own username of fly or other platform; change the "yourusername" in this file
   echo "🔐 docker login..."
   docker login
 else
-  echo "✅  Docker Hub user：your username"
+  echo "✅  Docker Hub user：yourusername"
 fi
 
 echo "🐳 build  Docker image and push to Docker Hub..."
-docker buildx build --platform linux/amd64 -t your username/aisuggestion:latest --push .
+docker buildx build --platform linux/amd64 -t yourusername/aisuggestion:latest --push .
 
 echo "🚁 Using Fly.io deploy command to deploy the service..."
-/Users/c/.fly/bin/fly deploy --image your username/aisuggestion:latest   # you should change the fly path into your local path ;you should use your own username of fly or other platform
-echo "🎉 deploy done, you can visit：https://aisuggestion.xxx.xx"
+/Users/c/.fly/bin/fly deploy --image yourusername/aisuggestion:latest   # you should change the fly path into your local path ;you should use your own username of fly or other platform
+echo "🎉 deploy done, you can visit：https://aisuggestion.xxx.xx"   #the host the platform allocate to your service
